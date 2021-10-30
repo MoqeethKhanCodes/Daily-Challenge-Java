de40f7227c793d46b9c593efad55eda49e2062d5
MoqeethKhanCodes <abdul.moqeethkhan@gmail.com>
2021-10-30 12:16:43 +0530

    Java Coding

--- BankAccountChallenge/src/com/moqeethcodes/Main.java
+++ BankAccountChallenge/src/com/moqeethcodes/Main.java
@@ -14,6 +14,7 @@ public class Main {
 //                   but not allow the withdrawal to complete if their are insufficient funds.
 
         //Replaced by Constructor
+        // Using setters
         /*moqeethsAccount.setAccountNumber(123456);
         moqeethsAccount.setBalance(0.00);
         moqeethsAccount.setCustomerName("Moqeeth Khan");
@@ -37,11 +38,34 @@ public class Main {
         //OR
 
         //Using Overloaded Constructor
-        BankAccount moqeeths1Account = new BankAccount(1548942L,15154214.82,"John Doe", "acs@xyz.com", "9425415421");
+        BankAccount moqeeths1Account = new BankAccount("1548942",15154214.82,"John Doe", "acs@xyz.com", "9425415421");
         System.out.println(moqeeths1Account.getAccountNumber());
         System.out.println(moqeeths1Account.getCustomerName());
         System.out.println(moqeeths1Account.getBalance());
         System.out.println(moqeeths1Account.getEmailId());
         System.out.println(moqeeths1Account.getPhoneNumber());
+
+        System.out.println("----------------------------------------");
+
+        //Ls 80
+        BankAccount ahmedscount = new BankAccount("Ahmed", "ahmed@gmail.com", "12354");
+        System.out.println(ahmedscount.getPhoneNumber() + " name " + ahmedscount.getCustomerName());
+
+        System.out.println("-----------VIP------------");
+
+        VipCustomer customer1 = new VipCustomer();
+        System.out.println(customer1.getName());
+
+        VipCustomer customer2 = new VipCustomer("Ahmed", 25000.00);
+        System.out.println(customer2.getName());
+
+
+        VipCustomer customer3 = new VipCustomer("Moqeeth ", 248000, "moqeeth@email.com");
+        System.out.println(customer3.getName());
+
+
+
+
+
     }
 }
