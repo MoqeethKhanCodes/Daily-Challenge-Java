eb2840f3a9909e95f75cc352a071795f79dd7818
Moqeeth <abdul.moqeethkhan@gmail.com>
2024-03-03 13:51:33 +0530

    Java 17 SwitchCalendarApplication

--- SwitchCalendarApplication/src/SwitchCalendar.java
+++ SwitchCalendarApplication/src/SwitchCalendar.java
@@ -1,10 +1,10 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-import java.util.logging.Logger;
 
 public class SwitchCalendar {
-    private static final Logger logger = Logger.getLogger(SwitchCalendar.class.getName());
+    //private static final Logger logger = Logger.getLogger(SwitchCalendar.class.getName());
 
     public static void takeInputFromUser () {
 
@@ -21,22 +21,38 @@ public class SwitchCalendar {
                 System.out.println("Please follow strict date format : dd month yyyy");
                 scanner.nextLine();
                 String date = scanner.nextLine();
-                Object [] dateFormatted = specificDateSearch(date);
+                Object [] dateObject = specificDateSearch(date);
+                printInfo(dateObject);
 
             }
-            case 2 -> System.out.println("CurrentDate");
+            case 2 -> {
+
+                LocalDateTime currentDateTime = LocalDateTime.now();
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+
+                String formattedDateTime = currentDateTime.format(formatter);
+
+                Object [] dateObject = specificDateSearch(formattedDateTime);
+
+                printInfo(dateObject);
+
+            }
         }
         scanner.close();
     }
 
-    public static Object[] specificDateSearch (String dateString) {
+    private static Object[] specificDateSearch (String dateString) {
         System.out.println("specificDateSearch");
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
 
         LocalDate date = LocalDate.parse(dateString, formatter);
 
+        return prepareDate(date);
+    }
 
+    private static Object[] prepareDate (LocalDate date) {
         Object [] object = new Object[4];
         object[0] = date.getDayOfMonth();
         object[1] = date.getMonth();
@@ -44,5 +60,9 @@ public class SwitchCalendar {
         object[3] = date.getDayOfWeek();
 
         return object;
+    }
+
+    private static void printInfo (Object[] dateInfo) {
+        System.out.println("Date Information : " + "\nDate : " + dateInfo[0] + " " + dateInfo[1] + " " + dateInfo[2] + " " + dateInfo[3]);
     }
 }
