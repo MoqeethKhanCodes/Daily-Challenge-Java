c09739950cda19439f5e9afedcc0a3a6a6256a28
Moqeeth <abdul.moqeethkhan@gmail.com>
2024-07-06 15:10:08 +0530

    Daily Coding : 05 July 2024 Revisiting Fundamentals

--- ClassesChallenge/src/Main.java
+++ ClassesChallenge/src/Main.java
@@ -4,12 +4,23 @@ public class Main {
         userAccount.setAccountNumber("1235564");
         userAccount.setAccountBalance(100);
         userAccount.setCustomerName("K");
-
+        userAccount.setEmail("k@email.com");
 
         userAccount.withdrawFunds(10);
         userAccount.depositFunds(100);
         userAccount.withdrawFunds(12.1);
 
-        System.out.println(userAccount.getAccountBalance());
+        System.out.println("--------------------------------");
+
+        Account userAccount1 = new Account();
+        userAccount1.setAccountNumber("8568564255");
+        userAccount1.setAccountBalance(0);
+        userAccount1.setCustomerName("M");
+        userAccount1.setEmail("M@email.com");
+        System.out.println(userAccount1.getAccountBalance());
+
+        userAccount1.withdrawFunds(10);
+        userAccount1.depositFunds(100);
+        userAccount1.withdrawFunds(12.1);
     }
 }
