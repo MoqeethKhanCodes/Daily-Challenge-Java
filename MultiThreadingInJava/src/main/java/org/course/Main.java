547cda6da3a73b79ed12f41814e9793cc506472d
Moqeeth <abdul.moqeethkhan@gmail.com>
2024-09-03 14:16:15 +0530

    Revisiting Fundamentals - 03 September 2024

--- MultiThreadingInJava/src/main/java/org/course/Main.java
+++ MultiThreadingInJava/src/main/java/org/course/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.course;
 
 public class Main {
     public static void main(String[] args) {
--- MultiThreadingInJava/src/main/java/org/course/MultiThreadingUsingImplements.java
+++ MultiThreadingInJava/src/main/java/org/course/MultiThreadingUsingImplements.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.course;
 
 public class MultiThreadingUsingImplements implements Runnable{
 
