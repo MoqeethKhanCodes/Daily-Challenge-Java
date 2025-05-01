1c46cc2b389d15d1302dbd9552e9b7e7d9ab9671
Moqeeth <abdul.moqeethkhan@gmail.com>
2025-05-01 18:19:10 +0530

    Revisiting Fundamentals - 01st May 2025

--- Collections/src/Main.java
+++ Collections/src/Main.java
@@ -20,7 +20,9 @@ public class Main {
         groceryObjectArray[0] = new GroceryItem("milk");
         groceryObjectArray[1] = new GroceryItem("apples","PRODUCE", 7);
         groceryObjectArray[2] = "6 Oranges";
-        System.out.println(Arrays.toString(groceryObjectArray));
+        System.out.println("groceryObjectArray : " + Arrays.toString(groceryObjectArray));
+
+        System.out.println("----------------");
 
         // Recommended to use specific type | As it allows Compile-Time type Checking
         GroceryItem[] groceryArray = new GroceryItem[3];
@@ -28,8 +30,9 @@ public class Main {
         groceryArray[1] = new GroceryItem("tomato","PRODUCE", 7);
         //groceryArray[2] = "6 Oranges"; Compile Time Error Checking
         groceryArray[2] = new GroceryItem("potato","PRODUCE", 10);
-        System.out.println(Arrays.toString(groceryArray));
+        System.out.println("groceryArray : " + Arrays.toString(groceryArray));
 
+        System.out.println("----------------");
         // Not Recommended | Raw use of parameterized class 'ArrayList' | Results in the compiler treating them as part Object class
         ArrayList objectList = new ArrayList();
         objectList.add(new GroceryItem("Butter"));
@@ -39,12 +42,25 @@ public class Main {
             System.out.println(o);
         }
 
+        System.out.println("----------------");
+
         // Recommended | Correct way to use Collections Classes
         ArrayList<GroceryItem> groceryList = new ArrayList<>(); // Here <> is called a Diamond Operator meaning Java can infer the type at Runtime
         groceryList.add(new GroceryItem("Cheese"));
         groceryList.add(new GroceryItem("banana","FRUITS", 12));
-        // Overloaded version add() method
+
+        // Overloaded add(index, element) method: Inserts the element at the specified index.
+        // Existing elements from that index onward are shifted one position to the right.
         groceryList.add(0,
                 new GroceryItem("Pears", "FRUITS", 3));
+
+        // The set(index, element) method replaces the element at the specified index.
+        // It does not shift any elements, unlike add(index, element).
+        groceryList.set(1,
+                new GroceryItem("pears", "FRUITS", 5));
+
+        groceryList.remove(2);
+
+        System.out.println("groceryList : " + groceryList);
     }
 }
